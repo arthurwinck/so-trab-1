725b817367bb926ff0c5c0d8aef753a031d473f5
Winckdev <apwinck@mail.com>
2022-04-28 17:38:00 -0300

    Adicionar métodos da biblioteca

--- cpu.cc
+++ cpu.cc
@@ -1,16 +1,19 @@
 #include "cpu.h"
 #include <iostream>
+#include "ucontext.h"
 
 __BEGIN_API
 
 void CPU::Context::save()
 {
     //adicionar implementação
+    getcontext(&this->_context);
 }
 
 void CPU::Context::load()
 {
     //adicionar implementação
+    setcontext(&this->_context);
 }
 
 CPU::Context::~Context()
@@ -21,6 +24,7 @@ CPU::Context::~Context()
 void CPU::switch_context(Context *from, Context *to)
 {
      //implementação do método
+     swapcontext(&from->_context, &to->_context);
 }
 
 __END_API
